d93cc431bed67bffc3ac85ea82bb4915cc6f5e1a
aufall02 <aufaluzumaki@gmail.com>
2023-04-16 09:08:42 +0700

    tugas praktikum day 1

---
+++ app/src/main/java/com/aufal/praktikumday1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.aufal.praktikumday1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText inNama, inNIM, inAlamat;
+    TextView viewNama, viewNIM, viewAlamat;
+    Button btnTampil;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inNama  = findViewById(R.id.inNama);
+        inNIM  = findViewById(R.id.inNIM);
+        inAlamat  = findViewById(R.id.inAlamat);
+        btnTampil = findViewById(R.id.btnTampil);
+        viewNama = findViewById(R.id.viewNama);
+        viewNIM = findViewById(R.id.viewNIM);
+        viewAlamat = findViewById(R.id.viewAlamat);
+
+
+        btnTampil.setOnClickListener(v ->{
+            String nama = inNama.getText().toString();
+            String nim = inNIM.getText().toString();
+            String alamat = inAlamat.getText().toString();
+            viewNama.setText(nama);
+            viewNIM.setText(nim);
+            viewAlamat.setText(alamat);
+            System.out.println(v);
+        });
+
+
+    }
+}
